f2cf4dc169b4c689ccfed74f8daabc0a5b60b458
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-08-26 14:19:58 +0000

    Should give better names for enum members...

--- branches/GPTP/drawing/src/graphics/graphics.h
+++ branches/GPTP/drawing/src/graphics/graphics.h
@@ -8,14 +8,15 @@ namespace graphics {
 const int MAX_SHAPES  = 10000;
 const int MAX_STRINGS = 2000;
 
-//TODO: Replace [toMap] with proper enums (CoordType?)
-void drawText(int x, int y, const std::string* str, bool toMap = true);
-void drawDot(int x, int y, u8 color, bool toMap = true);
-void drawLine(int x1, int y1, int x2, int y2, u8 color, bool toMap = true);
-void drawBox(int x1, int y1, int x2, int y2, u8 color, bool toMap = true);
-void drawRect(int x1, int y1, int width, int height, u8 color, bool toMap = true);
-void drawCircle(int x1, int y1, int radius, u8 color, bool toMap = true);
-void drawFilledBox(int x1, int y1, int x2, int y2, u8 color, bool toMap = true);
-void drawFilledCircle(int x1, int y1, int radius, u8 color, bool toMap = true);
+enum CoordType { SCREEN, MAP };
+
+void drawText(int x, int y, const std::string* str, CoordType target = SCREEN);
+void drawDot(int x, int y, u8 color, CoordType target = SCREEN);
+void drawLine(int x1, int y1, int x2, int y2, u8 color, CoordType target = SCREEN);
+void drawBox(int x1, int y1, int x2, int y2, u8 color, CoordType target = SCREEN);
+void drawRect(int x1, int y1, int width, int height, u8 color, CoordType target = SCREEN);
+void drawCircle(int x1, int y1, int radius, u8 color, CoordType target = SCREEN);
+void drawFilledBox(int x1, int y1, int x2, int y2, u8 color, CoordType target = SCREEN);
+void drawFilledCircle(int x1, int y1, int radius, u8 color, CoordType target = SCREEN);
 
 } //graphics
