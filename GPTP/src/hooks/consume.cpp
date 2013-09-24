459bbd2ed0ad6e7ede624fb51a409162b5620ce6
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-09-24 13:20:01 +0000

    Converted consume hooks to source/header/injector format. Fixed max unit energy hook to use u16 instead of s32. Fixed max unit energy hook to use static variables rather than mess with the stack.

--- GPTP/src/hooks/consume.cpp
+++ GPTP/src/hooks/consume.cpp
@@ -3,22 +3,31 @@
 #include "../SCBW/api.h"
 #include "../SCBW/scbwdata.h"
 #include "../SCBW/enumerations.h"
+#include <algorithm>
+
 
 /// Helper function: Sets death scores for [unit] owned by [player].
 void incrementDeathScores(const CUnit *unit, u8 player);
 
+namespace hooks {
 
 /// Decides whether the given unit can be Consumed.
 bool unitIsConsumable(const CUnit* unit, u8 consumingPlayer) {
-	using Unit::BaseProperty;
-	using Unit::GroupFlags;
-	using UnitProperty::Building;
-
 	//Default StarCraft behavior
-	return !(BaseProperty[unit->id] & Building)   //Cannot consume buildings
-         && unit->playerId == consumingPlayer	  //Must be owned by the current player
-         && GroupFlags[unit->id].isZerg		      //Must be a Zerg unit
-         && unit->id != UnitId::larva;					//Cannot consume larvae
+
+  //Cannot consume buildings
+  if (Unit::BaseProperty[unit->id] & UnitProperty::Building)
+    return false;
+  
+  //Must be owned by the current player
+  if (unit->playerId != consumingPlayer)
+    return false;
+
+  if (Unit::GroupFlags[unit->id].isZerg   //Must be a Zerg unit
+      && unit->id != UnitId::larva)			  //Cannot consume larvae
+    return true;
+
+  return false;
 }
 
 /// Is called when a unit is consumed.
@@ -28,22 +37,21 @@ void onConsumeUnit(CUnit* caster, CUnit *target) {
   target->remove();
   if (!(target->status & UnitStatus::IsHallucination)) {  //If not a hallucination
     u16 energy = caster->energy + 12800; //50 energy
-    u16 maxEnergy = getUnitMaxEnergy(caster);
-    if (energy > maxEnergy)
-      energy = maxEnergy;
-    caster->energy = energy;
+    caster->energy = std::min(energy, getUnitMaxEnergy(caster));
   }
 }
 
+} //hooks
 
-/**** Definitions of helper functions. Do not edit anything below this! ****/
+//-------- Helper function definitions. Do not edit anything below! --------//
 
+const u32 Func_IncrementDeathScores = 0x00488AF0;
 void incrementDeathScores(const CUnit *unit, u8 player) {
   __asm {
     PUSHAD
     MOV EDI, unit
     MOVZX EDX, player
-    CALL offsets::Util_IncrementDeathScores
+    CALL Func_IncrementDeathScores
     POPAD
   }
 }
--- GPTP/src/hooks/consume.h
+++ GPTP/src/hooks/consume.h
@@ -1,62 +1,11 @@
 #pragma once
-#include "../SCBW/structures.h"
+#include "../SCBW/structures/CUnit.h"
 
-namespace offsets {
-const u32 Hook_UnitIsConsumable   = 0x00491F51;
-const u32 Hook_OnConsumeUnit      = 0x004F47DE;
-const u32 Util_IncrementDeathScores = 0x00488AF0;
-}
+namespace hooks {
 
 bool unitIsConsumable(const CUnit* unit, u8 consumingPlayer);
 void onConsumeUnit(CUnit* caster, CUnit *target);
 
-//Inject with jmpPatch
-static void __declspec(naked) unitIsConsumableWrapper() {
-	CUnit* unit;
-	u8 defilerOwner;
-	__asm {
-		PUSHAD
-		MOV EBP, ESP
-		MOV unit, EAX
-		MOV defilerOwner, BL
-	}
+void injectConsumeHooks();
 
-	if (unitIsConsumable(unit, defilerOwner)) {
-		__asm {
-			POPAD
-			XOR AX, AX	;//Equivalent to MOV AX, 0
-			POP EBP
-			RETN 4
-		}
-	}
-	else {
-		__asm {
-			POPAD
-			MOV AX, 897	;//stat_txt.tbl string: Invalid target.<0>
-			POP EBP
-			RETN 4
-		}
-	}
-}
-
-//Inject with jmpPatch()
-static void __declspec(naked) onConsumeUnitWrapper() {
-	CUnit *caster, *target;
-
-	__asm {
-		PUSHAD
-		MOV EBP, ESP
-		MOV caster, ESI
-		MOV target, EDI
-	}
-
-	onConsumeUnit(caster, target);
-
-	__asm {
-	  POPAD
-	  POP EBX
-	  POP EDI
-	  RETN
-	}
-}
-
+} //hooks
