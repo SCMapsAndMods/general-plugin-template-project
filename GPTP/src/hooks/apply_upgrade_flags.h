c19201ca760cabddb9221c78fefa0f46369f52dc
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-09-24 12:38:10 +0000

    Converted Apply Upgrade Flags hook to source/header/injector format.

--- GPTP/src/hooks/apply_upgrade_flags.h
+++ GPTP/src/hooks/apply_upgrade_flags.h
@@ -1,46 +1,11 @@
 #pragma once
 #include "../SCBW/structures.h"
 
-namespace offsets {
-const u32 Hook_ApplyUpgradeFlagsToNewUnit       = 0x00454370;
-const u32 Hook_ApplyUpgradeFlagsToExistingUnits = 0x00454540;
-}
+namespace hooks {
 
 void applyUpgradeFlagsToNewUnitHook(CUnit* const unit);
 void applyUpgradeFlagsToExistingUnitsHook(const u8 playerId, const u8 upgradeId);
 
+void applyUpgradeFlagsInject();
 
-static void __declspec(naked) applyUpgradeFlagsToNewUnitWrapper() {
-  static CUnit *unit;
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV unit, ESI
-  }
-
-  applyUpgradeFlagsToNewUnitHook(unit);
-
-  __asm {
-    POPAD
-    RETN
-  }
-}
-
-static void __declspec(naked) applyUpgradeFlagsToExistingUnitsWrapper() {
-  static CUnit *unit;
-  static u8 upgradeId;
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV ECX, [EBP + 36]  ;//8 - 4 + 32
-    MOV unit, ECX
-    MOV upgradeId, AL
-  }
-
-  applyUpgradeFlagsToExistingUnitsHook(unit->playerId, upgradeId);
-
-  __asm {
-    POPAD
-    RETN 4
-  }
-}
+} //hooks
