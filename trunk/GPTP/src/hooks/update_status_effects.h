f40baa9ba6ff04a54080e8e96745c759793b6e6d
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-09-25 08:15:13 +0000

    Converted Update Status Effects hook to SHI format.

--- trunk/GPTP/src/hooks/update_status_effects.h
+++ trunk/GPTP/src/hooks/update_status_effects.h
@@ -1,36 +1,13 @@
 #pragma once
-#include "../SCBW/structures.h"
+#include "../SCBW/structures/CUnit.h"
 
-namespace offsets {
-const u32 Hook_UpdateStatusEffects  = 0x00492F70;
-}
+namespace hooks {
 
 void updateStatusEffectsHook(CUnit *unit);
 
-//Inject with jmpPatch()
-static void __declspec(naked) updateStatusEffectsWrapper() {
-  CUnit *unit;
-
-  __asm {
-    PUSHAD
-    MOV EBP, ESP
-    MOV unit, EAX
-  }
-
-  updateStatusEffectsHook(unit);
-
-  __asm {
-    POPAD
-    RETN
-  }
-}
+void injectUpdateStatusEffects();
 
 //Wrapper for native version of updateStatusEffects()
-static void updateStatusEffects(CUnit *unit) {
-  __asm {
-    PUSHAD
-    MOV EAX, unit
-    CALL offsets::Hook_UpdateStatusEffects
-    POPAD
-  }
-}
+void updateStatusEffects(CUnit *unit);
+
+} //hooks
