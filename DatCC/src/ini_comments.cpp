311e79d71ab4158337bf3fdc6e8c47f9cb7c13ca
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-10-03 22:12:20 +0000

    DatCC: Moved redundant code in comment maker functions to makeCommentPadding()

--- DatCC/src/ini_comments.cpp
+++ DatCC/src/ini_comments.cpp
@@ -6,68 +6,44 @@ namespace datcc {
 
 //-------- INI key/value comments --------//
 
-std::string makeUnitComment(int unitId, size_t keyStrSize) {
+std::string makeCommentPadding(int id, size_t keyStrSize) {
   std::ostringstream os;
-  os << unitId;
+  os << id;
   os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << getUnitName(unitId);
+  os << "; ";
   return os.str();
+}
+
+std::string makeUnitComment(int unitId, size_t keyStrSize) {
+  return makeCommentPadding(unitId, keyStrSize) + getUnitName(unitId);
 }
 
 std::string makeWeaponComment(int weaponId, size_t keyStrSize) {
-  std::ostringstream os;
-  os << weaponId;
-  os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << getWeaponName(weaponId);
-  return os.str();
+  return makeCommentPadding(weaponId, keyStrSize) + getWeaponName(weaponId);
 }
 
 std::string makeFlingyComment(int flingyId, size_t keyStrSize) {
-  std::ostringstream os;
-  os << flingyId;
-  os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << getFlingyName(flingyId);
-  return os.str();
+  return makeCommentPadding(flingyId, keyStrSize) + getFlingyName(flingyId);
 }
 
 std::string makeSpriteComment(int spriteId, size_t keyStrSize) {
-  std::ostringstream os;
-  os << spriteId;
-  os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << getSpriteName(spriteId);
-  return os.str();
+  return makeCommentPadding(spriteId, keyStrSize) + getSpriteName(spriteId);
 }
 
 std::string makeImageComment(int imageId, size_t keyStrSize) {
-  std::ostringstream os;
-  os << imageId;
-  os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << getImageName(imageId);
-  return os.str();
+  return makeCommentPadding(imageId, keyStrSize) + getImageName(imageId);
 }
 
 std::string makeOrderComment(int orderId, size_t keyStrSize) {
-  std::ostringstream os;
-  os << orderId;
-  os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << getOrderName(orderId);
-  return os.str();
+  return makeCommentPadding(orderId, keyStrSize) + getOrderName(orderId);
 }
 
 std::string makeUpgradeComment(int upgradeId, size_t keyStrSize) {
-  std::ostringstream os;
-  os << upgradeId;
-  os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << getUpgradeName(upgradeId);
-  return os.str();
+  return makeCommentPadding(upgradeId, keyStrSize) + getUpgradeName(upgradeId);
 }
 
 std::string makeTblComment(int stringIndex, size_t keyStrSize) {
-  std::ostringstream os;
-  os << stringIndex;
-  os.width(32 - os.tellp() - keyStrSize);
-  os << "; " << statTxtTbl.getEscapedString(stringIndex);
-  return os.str();
+  return makeCommentPadding(stringIndex, keyStrSize) + statTxtTbl.getEscapedString(stringIndex);
 }
 
 
