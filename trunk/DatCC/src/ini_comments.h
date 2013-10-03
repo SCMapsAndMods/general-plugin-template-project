5241bd41fa1611e6e21207d4ec09f1724b510ad9
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-10-03 18:32:50 +0000

    Fixed comment maker functions to be more efficient.

--- trunk/DatCC/src/ini_comments.h
+++ trunk/DatCC/src/ini_comments.h
@@ -7,12 +7,12 @@ std::string makeUnitSectionComment(int unitId);
 std::string makeWeaponSectionComment(int weaponId);
 std::string makeFlingySectionComment(int flingyId);
 
-std::string makeUnitComment(const std::string &iniKey, int unitId);
-std::string makeWeaponComment(const std::string &iniKey, int weaponId);
-std::string makeFlingyComment(const std::string &iniKey, int unitId);
-std::string makeSpriteComment(const std::string &iniKey, int spriteId);
-std::string makeImageComment(const std::string &iniKey, int imageId);
-std::string makeOrderComment(const std::string &iniKey, int orderId);
-std::string makeTblComment(const std::string &iniKey, int stringIndex);
+std::string makeUnitComment(int unitId, size_t keyStrSize);
+std::string makeWeaponComment(int weaponId, size_t keyStrSize);
+std::string makeFlingyComment(int unitId, size_t keyStrSize);
+std::string makeSpriteComment(int spriteId, size_t keyStrSize);
+std::string makeImageComment(int imageId, size_t keyStrSize);
+std::string makeOrderComment(int orderId, size_t keyStrSize);
+std::string makeTblComment(int stringIndex, size_t keyStrSize);
 
 } //datcc
