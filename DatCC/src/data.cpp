092a92006bb9271271d0fb30355a038447de7ac8
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-10-03 19:37:05 +0000

    DatCC:  * Added support for upgrades.dat  * TblFile.cpp no longer uses assert()s. Invalid TBL indexes simply return "invalid string index".

--- DatCC/src/data.cpp
+++ DatCC/src/data.cpp
@@ -6,9 +6,10 @@ namespace datcc {
 
 //-------- Default DAT paths --------//
 
-char DefaultDat<UnitsDat>::path[]   = "data/units.dat";
-char DefaultDat<WeaponsDat>::path[] = "data/weapons.dat";
-char DefaultDat<FlingyDat>::path[]  = "data/flingy.dat";
+char DefaultDat<UnitsDat>::path[]     = "data/units.dat";
+char DefaultDat<WeaponsDat>::path[]   = "data/weapons.dat";
+char DefaultDat<FlingyDat>::path[]    = "data/flingy.dat";
+char DefaultDat<UpgradesDat>::path[]  = "data/upgrades.dat";
 
 //-------- Current program directory --------//
 
@@ -33,7 +34,6 @@ const std::string& getCurrentProgramDir() {
 const int ORDER_TYPE_COUNT = 189;
 const int SPRITE_TYPE_COUNT = 517;
 const int IMAGE_TYPE_COUNT = 999;
-const int UPGRADE_TYPE_COUNT = 61;
 
 std::string unitNames[UNIT_TYPE_COUNT + 1];
 std::string weaponNames[WEAPON_TYPE_COUNT + 1];
--- DatCC/src/datcc.cpp
+++ DatCC/src/datcc.cpp
@@ -118,13 +118,17 @@ void compileWeapons(const std::string &inputPath) {
   compileDat<WeaponsDat>(inputPath);
 }
 
-void compileFlingy (const std::string &inputPath) {
+void compileFlingy(const std::string &inputPath) {
   compileDat<FlingyDat>(inputPath);
 }
 
 void compileSprites (const std::string &inputPath);
 void compileImages  (const std::string &inputPath);
-void compileUpgrades(const std::string &inputPath);
+
+void compileUpgrades(const std::string &inputPath) {
+  compileDat<UpgradesDat>(inputPath);
+}
+
 void compileTech    (const std::string &inputPath);
 void compileOrders  (const std::string &inputPath);
 
@@ -144,7 +148,11 @@ void decompileFlingy(const std::string &inputPath) {
 
 void decompileSprites (const std::string &inputPath);
 void decompileImages  (const std::string &inputPath);
-void decompileUpgrades(const std::string &inputPath);
+
+void decompileUpgrades(const std::string &inputPath) {
+  decompileDat<UpgradesDat>(inputPath);
+}
+
 void decompileTech    (const std::string &inputPath);
 void decompileOrders  (const std::string &inputPath);
 
