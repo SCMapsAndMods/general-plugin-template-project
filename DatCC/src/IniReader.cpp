5b9222b85a4d42209a76cc128819249f123c9790
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-10-07 16:18:16 +0000

    DatCC: IniReader now properly processes inline comments

--- DatCC/src/IniReader.cpp
+++ DatCC/src/IniReader.cpp
@@ -11,12 +11,4 @@ int IniReader::loadFrom(const std::string &fileName) {
   return ini.LoadFile(fileName.c_str());
 }
 
-//-------- Member function template specialization --------//
-
-template <>
-int IniReader::process(std::string &str, const std::string &key) {
-  str = ini.GetValue(currentSection.c_str(), key.c_str(), str.c_str());
-  return 0;
-}
-
 } //datcc
