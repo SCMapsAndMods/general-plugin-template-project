d44cc8c788bb338bce2ec0c02a22f76f74886b45
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-11-11 07:32:50 +0000

    Tagged GPTP v2.36 release

---
+++ release-2.36/src/AI/spells/optical_flare.cpp
@@ -0,0 +1,49 @@
+#include "psi_storm.h"
+
+namespace AI {
+
+class OpticalFlareTargetFinderProc: public scbw::UnitFinderCallbackMatchInterface {
+  private:
+    const CUnit *caster;
+    bool isUnderAttack;
+  public:
+    OpticalFlareTargetFinderProc(const CUnit *caster, bool isUnderAttack)
+      : caster(caster), isUnderAttack(isUnderAttack) {}
+
+    bool match(const CUnit *target) {
+      if (target == caster)
+        return false;
+
+      if (!isTargetWorthHitting(target, caster))
+        return false;
+
+      if (Unit::BaseProperty[target->id] & UnitProperty::Building)
+        return false;
+
+      if (target->isBlind)
+        return false;
+
+      if (target->canDetect())
+        return true;
+
+      if (getCurrentLifeInGame(target) > 80)
+        return true;
+
+      return false;
+    }
+};
+
+CUnit* findBestOpticalFlareTarget(const CUnit *caster, bool isUnderAttack) {
+  int bounds;
+  if (isUnderAttack)
+    bounds = 32 * 9;
+  else
+    bounds = 32 * 32;
+
+  return scbw::UnitFinder::getNearest(caster->getX(), caster->getY(),
+    caster->getX() - bounds, caster->getY() - bounds,
+    caster->getX() + bounds, caster->getY() + bounds,
+    OpticalFlareTargetFinderProc(caster, isUnderAttack));
+}
+
+} //AI
---
+++ release-2.36/src/Plugin.h
@@ -0,0 +1,16 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include "MPQDraftPlugin.h"
+
+class Plugin: public IMPQDraftPlugin {
+  public:
+    BOOL WINAPI Identify(LPDWORD lpdwPluginID);
+    BOOL WINAPI GetPluginName(LPSTR lpszPluginName, DWORD nNameBufferLength);
+    BOOL WINAPI CanPatchExecutable(LPCSTR lpszEXEFileName);
+    BOOL WINAPI Configure(HWND hParentWnd);
+    BOOL WINAPI ReadyForPatch();
+    BOOL WINAPI GetModules(MPQDRAFTPLUGINMODULE *lpPluginModules, LPDWORD lpnNumModules);
+    BOOL WINAPI InitializePlugin(IMPQDraftServer *lpMPQDraftServer);
+    BOOL WINAPI TerminatePlugin();
+};
