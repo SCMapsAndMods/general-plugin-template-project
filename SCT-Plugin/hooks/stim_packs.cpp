3241940daf1843d50da34d9814eb4b3231fa69ea
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-11-12 13:50:02 +0000

    SCT: Added 20-HP cost of Stim Packs for Fiends

--- SCT-Plugin/hooks/stim_packs.cpp
+++ SCT-Plugin/hooks/stim_packs.cpp
@@ -1,13 +1,21 @@
 #include "stim_packs.h"
 #include "../SCBW/api.h"
 
+const s32 getStimPacksHpCost(const CUnit *unit) {
+  if (unit->id == UnitId::firebat || unit->id == UnitId::gui_montag)
+    return 20 * 256;
+  else
+    return 10 * 256;
+}
+
 namespace hooks {
 
 void useStimPacksHook(CUnit *unit) {
-  //Default StarCraft behavior
-  if (unit->hitPoints > 2560) {
+  const s32 hpCost = getStimPacksHpCost(unit);
+
+  if (unit->hitPoints > hpCost) {
     scbw::playSound(scbw::randBetween(278, 279), unit);
-    unit->damageHp(2560);
+    unit->damageHp(hpCost);
     if (unit->stimTimer < 37) {
       unit->stimTimer = 37;
       unit->updateSpeed();
@@ -17,7 +25,7 @@ void useStimPacksHook(CUnit *unit) {
 
 bool canUseStimPacksHook(const CUnit *unit) {
   //Default StarCraft behavior
-  return unit->hitPoints > 2560;
+  return unit->hitPoints > getStimPacksHpCost(unit);
 }
 
 } //hooks
