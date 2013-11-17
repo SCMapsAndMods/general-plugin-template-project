762828c367ad88cf8d0c48066ea613a7f47ba27e
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-11-17 17:46:09 +0000

    Undo mistake in spawning a new project

--- trunk/AiResourceInflater/src/AI/spells/feedback.h
+++
@@ -1,7 +0,0 @@
-#include <AI/ai_common.h>
-
-namespace AI {
-
-CUnit* findBestFeedbackTarget(const CUnit *caster, bool isUnderAttack);
-
-} //AI
--- trunk/AiResourceInflater/src/AI/spells/launch_nuke.h
+++
@@ -1,7 +0,0 @@
-#include <AI/ai_common.h>
-
-namespace AI {
-
-CUnit* findBestNukeLaunchTarget(const CUnit *caster, bool isUnderAttack);
-
-} //AI
