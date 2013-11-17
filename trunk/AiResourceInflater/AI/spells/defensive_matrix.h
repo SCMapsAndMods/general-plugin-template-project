392bf5319cd4178d062ed5ee212e9b3fc95ef014
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-11-17 18:34:47 +0000

    Spawn again, AI Resource Inflater plugin project

---
+++ trunk/AiResourceInflater/AI/spells/defensive_matrix.h
@@ -0,0 +1,7 @@
+#include <AI/ai_common.h>
+
+namespace AI {
+
+CUnit* findBestDefensiveMatrixTarget(const CUnit *caster, bool isUnderAttack);
+
+} //AI
---
+++ trunk/AiResourceInflater/AI/spells/feedback.cpp
@@ -0,0 +1,51 @@
+#include "psi_storm.h"
+
+namespace AI {
+
+class FeedbackTargetFinderProc: public scbw::UnitFinderCallbackMatchInterface {
+  private:
+    const CUnit *caster;
+    bool isUnderAttack;
+  public:
+    FeedbackTargetFinderProc(const CUnit *caster, bool isUnderAttack)
+      : caster(caster), isUnderAttack(isUnderAttack) {}
+
+    bool match(const CUnit *target) {
+      if (target == caster)
+        return false;
+
+      if (!isTargetWorthHitting(target, caster))
+        return false;
+
+      if (!(Unit::BaseProperty[target->id] & UnitProperty::Spellcaster))
+        return false;
+
+      if (target->status & (UnitStatus::IsHallucination | UnitStatus::GroundedBuilding))
+        return false;
+
+      if (Unit::BaseProperty[target->id] & UnitProperty::Hero)
+        return false;
+
+      if (target->energy / 256 >= getCurrentLifeInGame(target))
+        return true;
+
+      return false;
+    }
+};
+
+CUnit* findBestFeedbackTarget(const CUnit *caster, bool isUnderAttack) {
+  int bounds;
+  if (isUnderAttack)
+    bounds = 32 * 9;
+  else if (isUmsMode(caster->playerId))
+    bounds = 32 * 64;
+  else
+    bounds = 32 * 32;
+
+  return scbw::UnitFinder::getNearest(caster->getX(), caster->getY(),
+    caster->getX() - bounds, caster->getY() - bounds,
+    caster->getX() + bounds, caster->getY() + bounds,
+    FeedbackTargetFinderProc(caster, isUnderAttack));
+}
+
+} //AI
