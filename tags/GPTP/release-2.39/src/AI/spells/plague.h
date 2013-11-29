011197e1c04fc348f1576e33a4681fcd10cf3a76
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-11-29 18:37:15 +0000

    Tag v2.39 release

---
+++ tags/GPTP/release-2.39/src/AI/spells/plague.h
@@ -0,0 +1,7 @@
+#include <AI/ai_common.h>
+
+namespace AI {
+
+CUnit* findBestPlagueTarget(const CUnit *caster, bool isUnderAttack);
+
+} //AI
---
+++ tags/GPTP/release-2.39/src/hooks/apply_upgrade_flags.h
@@ -0,0 +1,16 @@
+//Header file for the Apply Upgrade Flags hook module.
+#pragma once
+#include <SCBW/structures.h>
+
+namespace hooks {
+
+void applyUpgradeFlagsToNewUnitHook(CUnit *unit);
+void applyUpgradeFlagsToExistingUnitsHook(u8 playerId, u8 upgradeId);
+
+void injectApplyUpgradeFlags();
+
+//Wrapper for the native version of applyUpgradeFlagsToExistingUnits().
+//Applies @upgradeId to all units that are owned by the owner of @p unit.
+void applyUpgradeFlagsToExistingUnits(CUnit *unit, u8 upgradeId);
+
+} //hooks
