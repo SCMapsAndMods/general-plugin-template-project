0db82be0a86131ce35378bda9e504f23ab1dc2bd
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-12-11 21:10:30 +0000

    Stim timer fix

--- trunk/GutsAndMaggots/hooks/game_hooks.cpp
+++ trunk/GutsAndMaggots/hooks/game_hooks.cpp
@@ -9,8 +9,6 @@
 #include <cstdio>
 
 bool firstRun = true;
-//자원 제공 및 텍스트 효과를 위한 타이머
-u32 extractorTimer;
 
 namespace hooks {
 
@@ -22,7 +20,6 @@ bool nextFrame() {
 
     if (firstRun) {
       firstRun = false;
-      extractorTimer = 0;
     }
 
     // Loop through the unit table.
@@ -33,16 +30,16 @@ bool nextFrame() {
     //  //Write your code here
     //}
 
-    ++extractorTimer;
-    u32 extractorTimerTick = extractorTimer % 15; //1초 == 15프레임
-
     // Alternative looping method
     // Guarantees that [unit] points to an actual unit.
     for (CUnit *unit = *firstVisibleUnit; unit; unit = unit->next) {
       //익스트랙터에서 매초마다 자원 제공
       if (unit->id == UnitId::extractor && unit->status & UnitStatus::Completed)
       {
-        if (extractorTimerTick == 0) {
+        //스팀팩 타이머를 재활용
+        unit->stimTimer++;
+        if (unit->stimTimer >= 15) {
+          unit->stimTimer = 0;
           resources->minerals[unit->playerId]           += EXTRACTOR_MINERALS;
           resources->gas[unit->playerId]                += EXTRACTOR_GAS;
           resources->cumulativeMinerals[unit->playerId] += EXTRACTOR_MINERALS;
@@ -53,7 +50,7 @@ bool nextFrame() {
         const char extractorResourceText[] = "\x1F미네랄 +" STR(EXTRACTOR_MINERALS) " \x07가스 +" STR(EXTRACTOR_GAS);
         
         int textX = unit->getX() - 40;
-        int textY = unit->getY() - 30 - extractorTimerTick / 2;
+        int textY = unit->getY() - 30 - (unit->stimTimer / 2);
 
         graphics::drawText(textX, textY, extractorResourceText,
           graphics::FONT_MEDIUM, graphics::ON_MAP);
