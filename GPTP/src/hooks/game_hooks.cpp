a414ccb831bdd3ae456f5f9f6f5174fc3a997073
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-12-15 16:37:33 +0000

    GPTP:  * Remove the long-overused and mistreated firstRun variable, replacing it with an elapsedTimeFrames check  * Remove the long-misused unitTable and bulletTable variables.

--- GPTP/src/hooks/game_hooks.cpp
+++ GPTP/src/hooks/game_hooks.cpp
@@ -8,7 +8,6 @@
 #include "psi_field.h"
 #include <cstdio>
 
-bool firstRun = true;
 
 namespace hooks {
 
@@ -18,35 +17,16 @@ bool nextFrame() {
     graphics::resetAllGraphics();
     hooks::updatePsiFieldProviders();
 
-    if (firstRun) {
+    if (*elapsedTimeFrames == 0) {
       scbw::printText("Hello, world!");
-      firstRun = false;
     }
 
-    // Loop through the unit table.
-    // Warning: There is no guarantee that the current unit is actually a unit
-    // rather than an unused space in memory.
-    //for (int i = 0; i < UNIT_ARRAY_LENGTH; ++i) {
-    //  CUnit* unit = &unitTable[i];
-    //  //Write your code here
-    //}
-
-    // Alternative looping method
-    // Guarantees that [unit] points to an actual unit.
+    // Loop through all visible units in the game.
     for (CUnit *unit = *firstVisibleUnit; unit; unit = unit->next) {
       //Write your code here
     }
 
-    // Loop through the bullet table.
-    // Warning: There is no guarantee that the current bullet is actually a
-    // bullet rather than an unused space in memory
-    //for (int i = 0; i < BULLET_ARRAY_LENGTH; ++i) {
-    //  BULLET* bullet = &bulletTable[i];
-    //  //Write your code here
-    //}
-
-    // Alternative looping method
-    // Guarantees that [bullet] points to an actual bullet.
+    // Loop through all bullets in the game
     //for (BULLET* bullet = *firstBullet; bullet; bullet = bullet->next) {
     //  //Write your code here
     //}
@@ -55,7 +35,6 @@ bool nextFrame() {
 }
 
 bool gameOn() {
-  firstRun = true;
   return true;
 }
 
