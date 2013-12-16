1af9130283593f9158b6671eb318d164d14c49d0
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2013-12-16 10:10:27 +0000

    (Belatedly) Create weapon_hooks branch

--- GagManiaDraftPlugin/AI/spells/spawn_broodlings.h
+++
@@ -1,7 +0,0 @@
-#include <AI/ai_common.h>
-
-namespace AI {
-
-CUnit* findBestSpawnBroodlingsTarget(const CUnit *caster, bool isUnderAttack);
-
-} //AI
--- GutsAndMaggots/AI/spells/mind_control.h
+++
@@ -1,7 +0,0 @@
-#include <AI/ai_common.h>
-
-namespace AI {
-
-CUnit* findBestMindControlTarget(const CUnit *caster, bool isUnderAttack);
-
-} //AI
