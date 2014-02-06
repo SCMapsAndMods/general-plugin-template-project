c8838e3cd0ae5af86f4456305247a8a37184a51f
philmasterplus@gmail.com <philmasterplus@gmail.com@0d0d0785-0381-f6c9-d033-10ae84f6be73>
2014-02-06 10:18:39 +0000

    GPTP:  * Reorganized SCBW/api.h and SCBW/api.cpp  * Add small comment to CUnit.h

--- GPTP/src/SCBW/api.cpp
+++ GPTP/src/SCBW/api.cpp
@@ -1,5 +1,4 @@
 #include "api.h"
-#include "scbwdata.h"
 #include <SCBW/UnitFinder.h>
 #include <algorithm>
 #include <cassert>
@@ -10,6 +9,21 @@
 
 
 namespace scbw {
+
+//-------- Output functions --------//
+
+const u32 Func_PlaySound = 0x0048ED50;
+void playSound(u32 sfxId, const CUnit *sourceUnit) {
+  __asm {
+    PUSHAD
+    PUSH 0
+    PUSH 1
+    MOV ESI, sourceUnit
+    MOV EBX, sfxId
+    CALL Func_PlaySound
+    POPAD
+  }
+}
 
 const u32 Func_PrintText = 0x0048CD30;
 void printText(const char* text, u32 color) {
@@ -27,19 +41,6 @@ void printText(const char* text, u32 color) {
   }
 }
 
-const u32 Func_PlaySound = 0x0048ED50;
-void playSound(u32 sfxId, const CUnit *sourceUnit) {
-  __asm {
-    PUSHAD
-    PUSH 0
-    PUSH 1
-    MOV ESI, sourceUnit
-    MOV EBX, sfxId
-    CALL Func_PlaySound
-    POPAD
-  }
-}
-
 const u32 Func_ShowErrorMessageWithSfx = 0x0048EE30;
 void showErrorMessageWithSfx(u32 playerId, u32 statTxtId, u32 sfxId) {
   __asm {
@@ -52,16 +53,23 @@ void showErrorMessageWithSfx(u32 playerId, u32 statTxtId, u32 sfxId) {
   }
 }
 
-u32 getUnitOverlayAdjustment(const CUnit* const unit) {
-  if (units_dat::BaseProperty[unit->id] & UnitProperty::MediumOverlay)
-    return 1;
-  else if (units_dat::BaseProperty[unit->id] & UnitProperty::LargeOverlay)
-    return 2;
-  else
-    return 0;
-}
+//-------- Unit checks --------//
 
-//-------- Weapon related --------//
+const u32 Func_CanBeEnteredBy = 0x004E6E00; //AKA CanEnterTransport()
+bool canBeEnteredBy(const CUnit* transport, const CUnit* unit) {
+  u32 result;
+
+  __asm {
+    PUSHAD
+    MOV EAX, transport
+    PUSH unit
+    CALL Func_CanBeEnteredBy
+    MOV result, EAX
+    POPAD
+  }
+
+  return result != 0;
+}
 
 //Identical to function @ 0x00475CE0
 bool canWeaponTargetUnit(u8 weaponId, const CUnit *target, const CUnit *attacker) {
@@ -101,43 +109,6 @@ bool canWeaponTargetUnit(u8 weaponId, const CUnit *target, const CUnit *attacker
   return true;
 }
 
-const u32 Func_CreateUnitAtPos = 0x004CD360; //AKA createUnitXY()
-CUnit* createUnitAtPos(u16 unitType, u16 playerId, u32 x, u32 y) {
-  if (unitType >= UNIT_TYPE_COUNT) return NULL;
-  CUnit* unit;
-
-  __asm {
-    PUSHAD
-    MOV CX, unitType
-    MOV AX, playerId
-    PUSH y
-    PUSH x
-    CALL Func_CreateUnitAtPos
-    MOV unit, EAX
-    POPAD
-  }
-
-  return unit;
-}
-
-const u32 Func_CanBeEnteredBy = 0x004E6E00; //AKA CanEnterTransport()
-bool canBeEnteredBy(const CUnit* transport, const CUnit* unit) {
-  u32 result;
-
-  __asm {
-    PUSHAD
-    MOV EAX, transport
-    PUSH unit
-    CALL Func_CanBeEnteredBy
-    MOV result, EAX
-    POPAD
-  }
-
-  return result != 0;
-}
-
-//-------- isUnderDarkSwarm() --------//
-
 bool isUnderDarkSwarm(const CUnit *unit) {
   static UnitFinder darkSwarmFinder;
 
@@ -148,8 +119,10 @@ bool isUnderDarkSwarm(const CUnit *unit) {
   return darkSwarm != nullptr;
 }
 
-// Improved code from BWAPI's include/BWAPI/Position.h: getApproxDistance()
-// Logically same as function @ 0x0040C360
+//-------- Graphics and geometry --------//
+
+//Improved code from BWAPI's include/BWAPI/Position.h: getApproxDistance()
+//Logically same as function @ 0x0040C360
 u32 getDistanceFast(s32 x1, s32 y1, s32 x2, s32 y2) {
   int dMax = abs(x1 - x2), dMin = abs(y1 - y2);
   if (dMax < dMin)
@@ -217,24 +190,18 @@ s32 getPolarY(s32 distance, u8 angle) {
   return distance * angleDistance[angle].y >> 8;
 }
 
-u8 getUpgradeLevel(u8 playerId, u8 upgradeId) {
-  assert(playerId < PLAYER_COUNT);
-  assert(upgradeId < UpgradeId::None);
+//-------- Player information --------//
 
-  if (upgradeId < UpgradeId::UnusedUpgrade46)
-    return UpgradesSc->currentLevel[playerId][upgradeId];
+s32 getSupplyRemaining(u8 playerId, u8 raceId) {
+  assert(raceId <= 2);
+  assert(playerId < 12);
+
+  s32 supplyProvided;
+  if (isCheatEnabled(CheatFlags::FoodForThought))
+    supplyProvided = raceSupply[raceId].max[playerId];
   else
-    return UpgradesBw->currentLevel[playerId][upgradeId - UpgradeId::UnusedUpgrade46];
-}
-
-void setUpgradeLevel(u8 playerId, u8 upgradeId, u8 level) {
-  assert(playerId < PLAYER_COUNT);
-  assert(upgradeId < UpgradeId::None);
-
-  if (upgradeId < UpgradeId::UnusedUpgrade46)
-    UpgradesSc->currentLevel[playerId][upgradeId] = level;
-  else
-    UpgradesBw->currentLevel[playerId][upgradeId - UpgradeId::UnusedUpgrade46] = level;
+    supplyProvided = std::min(raceSupply[raceId].max[playerId], raceSupply[raceId].provided[playerId]);
+  return supplyProvided - raceSupply[raceId].used[playerId];
 }
 
 bool hasTechResearched(u8 playerId, u16 techId) {
@@ -257,17 +224,27 @@ void setTechResearchState(u8 playerId, u16 techId, bool isResearched) {
     TechBw->isResearched[playerId][techId - TechId::Restoration] = isResearched;
 }
 
-s32 getSupplyRemaining(u8 playerId, u8 raceId) {
-  assert(raceId <= 2);
-  assert(playerId < 12);
+u8 getUpgradeLevel(u8 playerId, u8 upgradeId) {
+  assert(playerId < PLAYER_COUNT);
+  assert(upgradeId < UpgradeId::None);
 
-  s32 supplyProvided;
-  if (isCheatEnabled(CheatFlags::FoodForThought))
-    supplyProvided = raceSupply[raceId].max[playerId];
+  if (upgradeId < UpgradeId::UnusedUpgrade46)
+    return UpgradesSc->currentLevel[playerId][upgradeId];
   else
-    supplyProvided = std::min(raceSupply[raceId].max[playerId], raceSupply[raceId].provided[playerId]);
-  return supplyProvided - raceSupply[raceId].used[playerId];
+    return UpgradesBw->currentLevel[playerId][upgradeId - UpgradeId::UnusedUpgrade46];
 }
+
+void setUpgradeLevel(u8 playerId, u8 upgradeId, u8 level) {
+  assert(playerId < PLAYER_COUNT);
+  assert(upgradeId < UpgradeId::None);
+
+  if (upgradeId < UpgradeId::UnusedUpgrade46)
+    UpgradesSc->currentLevel[playerId][upgradeId] = level;
+  else
+    UpgradesBw->currentLevel[playerId][upgradeId - UpgradeId::UnusedUpgrade46] = level;
+}
+
+//-------- Map information --------//
 
 const u32 Func_GetGroundHeightAtPos = 0x004BD0F0;
 u32 getGroundHeightAtPos(s32 x, s32 y) {
@@ -283,6 +260,36 @@ u32 getGroundHeightAtPos(s32 x, s32 y) {
   }
 
   return height;
+}
+
+//-------- Utility functions --------//
+
+const u32 Func_CreateUnitAtPos = 0x004CD360; //AKA createUnitXY()
+CUnit* createUnitAtPos(u16 unitType, u16 playerId, u32 x, u32 y) {
+  if (unitType >= UNIT_TYPE_COUNT) return NULL;
+  CUnit* unit;
+
+  __asm {
+    PUSHAD
+    MOV CX, unitType
+    MOV AX, playerId
+    PUSH y
+    PUSH x
+    CALL Func_CreateUnitAtPos
+    MOV unit, EAX
+    POPAD
+  }
+
+  return unit;
+}
+
+u32 getUnitOverlayAdjustment(const CUnit* const unit) {
+  if (units_dat::BaseProperty[unit->id] & UnitProperty::MediumOverlay)
+    return 1;
+  else if (units_dat::BaseProperty[unit->id] & UnitProperty::LargeOverlay)
+    return 2;
+  else
+    return 0;
 }
 
 void refreshScreen(int left, int top, int right, int bottom) {
@@ -306,6 +313,22 @@ void refreshScreen() {
   memset(refreshRegions, 1, 1200);
 }
 
+u32 randBetween(u32 min, u32 max) {
+  assert(min <= max);
+  return min + ((max - min + 1) * random() >> 15);
+}
+
+u16 random() {
+  if (*IS_IN_GAME_LOOP) {
+    *lastRandomNumber = 22695477 * (*lastRandomNumber) + 1;
+    return (*lastRandomNumber >> 16) % 32768;  //Make a number between 0 and 32767
+  }
+  else
+    return 0;
+}
+
+//-------- Needs research --------//
+
 //Logically equivalent to function @ 0x004C36C0
 void refreshConsole() {
   u32*  const bCanUpdateCurrentButtonSet      = (u32*)  0x0068C1B0;
@@ -319,20 +342,6 @@ void refreshConsole() {
   *bCanUpdateStatDataDialog = 1;
   *someDialogUnknown = 0;
   *unknown2 = 0;
-}
-
-u16 random() {
-  if (*IS_IN_GAME_LOOP) {
-    *lastRandomNumber = 22695477 * (*lastRandomNumber) + 1;
-    return (*lastRandomNumber >> 16) % 32768;  //Make a number between 0 and 32767
-  }
-  else
-    return 0;
-}
-
-u32 randBetween(u32 min, u32 max) {
-  assert(min <= max);
-  return min + ((max - min + 1) * random() >> 15);
 }
 
 } //scbw
